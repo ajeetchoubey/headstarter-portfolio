3e6778bbeb2c5ab15b4393c4d35f2cf5bbed57bc
Ajeet <choubeyajeet0@gmail.com>
2024-07-26 05:53:49 +0530

    website completed

---
+++ src/data/data.js
@@ -0,0 +1,37 @@
+import noteSpot from '../assets/projects/noteSpot.png'
+import tubeFetch from '../assets/projects/tubeFetch.png'
+import TypingTest from "../assets/projects/TypingTest.png"
+import Frienderrr from "../assets/projects/Frienderrr.png"
+
+export const data=[
+    {
+        id:1,
+        name:"NoteSpot",
+        image:noteSpot,
+        github:"",
+        live:"https://notespotapp.netlify.app/",
+    },
+    {
+        id:2,
+        name:"TubeFetch",
+        image:tubeFetch,
+        github:"",
+        live:"https://tubefetch1.netlify.app/",
+    },
+    {
+        id:3,
+        name:"Frienderrr",
+        image: Frienderrr,
+        github:"https://github.com/ajeetchoubey/Frienderrr",
+        live:"",
+    },
+    {
+        id:4,
+        name:"Typing Speed Test",
+        image: TypingTest,
+        github:"",
+        live:"https://ajeetchoubey.github.io/Typing-Speed-Test/",
+    },
+
+
+]
